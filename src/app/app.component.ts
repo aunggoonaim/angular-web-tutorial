9306a653db3f8df4761763132ece315434e315b9
Aunggoon Arpasunun <rabbit.dev.group@gmail.com>
2022-12-17 09:53:32 +0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-web-tutorial';
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  message = [this.items.map(x => null)];
+
+  testClick() { 
+    alert(this.message[0]);
+  }
 }
